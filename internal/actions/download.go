f7c8d0f7d346fd750ffc11a94e343b04cde2db59
Alexandre Figueiredo <alexandre@blackfoot.io>
2021-03-05 21:46:29 +0100

    feat: add message to download file

--- internal/actions/download.go
+++ internal/actions/download.go
@@ -1,9 +1,6 @@
 package actions
 
 import (
-	"fmt"
-	"log"
-
 	"github.com/thelittlebigbot/ohmyscan/internal/utils"
 	"github.com/urfave/cli/v2"
 )
@@ -15,14 +12,14 @@ func Download(c *cli.Context) {
 	number := c.String("number")
 
 	if platform == "" || manga == "" || number == "" {
-		log.Fatal("You need to specify the platform, the manga and the number")
-	} else if platform == utils.ScanOPName {
-		platform := utils.ScanOPName
-		url := utils.ScanOPURL + manga + "/" + number
+		utils.Message(utils.ErrorArgumentsEmpty, "error")
+	} else if platform == utils.PlatformScanOPName {
+		platform := utils.PlatformScanOPName
+		url := utils.PlatformScanOPURL + manga + "/" + number
 
 		utils.DownloadFile(url, platform, manga, number)
 
-	} else if platform == utils.JapScanName {
-		fmt.Println("Comming soon from japscan") // TODO: Download files from japscan
+	} else if platform == utils.PlatformJapScanName {
+		utils.Message("Comming soon from Japscan.", "debug")
 	}
 }
