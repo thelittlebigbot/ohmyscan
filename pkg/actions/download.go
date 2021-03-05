3279befc431bb7770657c41ea2a26081add1f164
Alexandre Figueiredo <alexandre@blackfoot.io>
2021-03-05 18:42:39 +0100

    feat: download content from scan-op

--- pkg/actions/download.go
+++ pkg/actions/download.go
@@ -3,7 +3,9 @@ package actions
 import (
 	"fmt"
 	"log"
+	"strings"
 
+	"github.com/thelittlebigbot/ohmyscan/pkg/utils"
 	"github.com/urfave/cli/v2"
 )
 
@@ -15,7 +17,29 @@ func Download(c *cli.Context) {
 
 	if platform == "" || manga == "" || number == "" {
 		log.Fatal("You need to specify the platform, the manga and the number")
-	}
+	} else if platform == "scan-op" {
+		var url = "https://scan-op.cc/manga/" + manga + "/" + number
 
-	fmt.Println(platform, manga, number)
+		html := utils.GetContent(url)
+		imgs := utils.GetLinks(html, "scan-op")
+		if imgs == nil {
+			log.Fatal("No images provided")
+		}
+
+		utils.CreateFolder(url)
+
+		for _, elem := range imgs {
+			split := strings.Split(elem, "/")
+			name := "downloads/" + manga + "/" + number + "/" + split[(len(split)-1)]
+			name = strings.TrimSpace(name)
+			fmt.Println(split, name)
+
+			err := utils.GetFile(strings.TrimSpace(elem), name)
+			if err != nil {
+				log.Fatal(err)
+			} else {
+				fmt.Printf("%s is downloaded.\n", elem)
+			}
+		}
+	}
 }
