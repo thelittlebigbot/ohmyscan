636037962728b3c7742a11f159fea309f4940a57
Alexandre Figueiredo <alexandre@blackfoot.io>
2021-03-05 19:47:28 +0100

    feat: delete commands file and put content in main file

--- main.go
+++ main.go
@@ -1,7 +1,61 @@
 package main
 
-import "github.com/thelittlebigbot/ohmyscan/pkg/actions"
+import (
+	"log"
+	"os"
+
+	"github.com/thelittlebigbot/ohmyscan/internal/actions"
+	"github.com/urfave/cli/v2"
+)
 
 func main() {
-	actions.Commands()
+	app := &cli.App{
+		Name:  "Oh My Scan",
+		Usage: "Download locally your favorite french scans",
+	}
+
+	app.Commands = []*cli.Command{
+		{
+			Name:  "download",
+			Usage: "Download", // TODO: Add description
+			Flags: []cli.Flag{
+				&cli.StringFlag{
+					Name:  "platform",
+					Usage: "Select your platform",
+				}, &cli.StringFlag{
+					Name:  "manga",
+					Usage: "Select your manga",
+				}, &cli.StringFlag{
+					Name:  "number",
+					Usage: "Select your manga",
+				},
+			},
+			Action: func(c *cli.Context) error {
+				actions.Download(c)
+				return nil
+			},
+		},
+		{
+			Name:  "merge",
+			Usage: "Merge", // TODO: Add description
+			Flags: []cli.Flag{
+				&cli.StringFlag{
+					Name:  "manga",
+					Usage: "Select your manga",
+				}, &cli.StringFlag{
+					Name:  "number",
+					Usage: "Select your manga",
+				},
+			},
+			Action: func(c *cli.Context) error {
+				actions.Merge(c)
+				return nil
+			},
+		},
+	}
+
+	err := app.Run(os.Args)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
