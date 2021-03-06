dd79b9cc53dda456ff16e0abe06bdbc06cd77f37
Alexandre Figueiredo <alexandre@blackfoot.io>
2021-03-06 15:59:35 +0100

    feat: update docstrings

--- internal/actions/download.go
+++ internal/actions/download.go
@@ -5,7 +5,8 @@ import (
 	"github.com/urfave/cli/v2"
 )
 
-// Download ...
+// Download ... Function that allows us to download the desired scans according to the given parameters.
+// 	- c(*cli.Content): Pass the application context.
 func Download(c *cli.Context) {
 	platform := c.String("platform")
 	name := c.String("name")
--- internal/actions/merge.go
+++ internal/actions/merge.go
@@ -5,7 +5,8 @@ import (
 	"github.com/urfave/cli/v2"
 )
 
-// Merge ...
+// Merge ... Function that allows us to merge the downloaded scans according to the given parameters.
+// 	- c(*cli.Content): Pass the application context.
 func Merge(c *cli.Context) {
 	name := c.String("name")
 	number := c.String("number")
